f568b1f38fad5a7cebbcfcf1c3783b97d394cf90
olympiawoj <owojcik7@gmail.com>
2021-03-16 08:15:49 -0400

    Choosing a Date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const calendarDates = Array(31)
@@ -6,20 +6,49 @@ const calendarDates = Array(31)
   .map((e, i) => i);
 
 export default function App() {
+  const [choosingType, setChoosingType] = useState('start')
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+
+  function updateDate(chosenDay) {
+    // handle if a user chose before our current range
+    if (startDate && chosenDay < startDate){
+      setStartDate(chosenDay)
+      return setChoosingType('end')
+    }
+    // handle if a user chose after our current range
+    if(endDate && chosenDay > endDate){
+      setEndDate(chosenDay)
+      return setChoosingType('end')
+    }
+    
+    if(choosingType === 'start'){
+      setStartDate(chosenDay)
+      return setChoosingType('end');
+    } 
+    if(choosingType === 'end'){
+      setEndDate(chosenDay)
+    }
+  }
+
   return (
     <>
       <div className="date-chooser">
-        <button className="date-chooser-button">
-          Start Date <span>0</span>
+        <button className="date-chooser-button" onClick={()=>setChoosingType('start')}>
+          Start Date <span>{startDate}</span>
         </button>
-        <button className="date-chooser-button">
-          End Date <span>0</span>
+        <button className="date-chooser-button" onClick={()=>setChoosingType('end')}>
+          End Date <span>{endDate}</span>
         </button>
       </div>
 
       <div className="calendar">
         {calendarDates.map((day, index) => {
-          return <div className="calendar-day">{day + 1}</div>;
+          const dayNumber = day + 1;
+          return <button
+            key={index}
+            onClick={() => updateDate(dayNumber)}
+            className="calendar-day">{dayNumber}</button>;
         })}
       </div>
     </>
